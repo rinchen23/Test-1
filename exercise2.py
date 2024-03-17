3eb64581f99054d1e4b8f306edd2b2a27afd2f5f
rinchen23 <02230223.cst@rub.edu.bt>
2024-03-17 22:04:23 +0530

    exercise 2

--- exercise2.py
+++ exercise2.py
@@ -1,26 +1,44 @@
 from queue import Queue
-patient = Queue
-current_patient = None
-while True:
-    print(""" Desk manager: Select the option below:
-          1.Register patent
-          2.Remove patient
-          3.Display patient queue
-          4.Exit """)
-userinput = input(">>>")
-print()
-if userinput == "1":
-    patient_name = input("Enter patient name:")
-    patient.put(Patient_name)
-    current_patient = patient_name
-    print(f"patient {patient_name} successfully registered")
-elif userinput == "2":
-    patient.get()
-    print(f"patient {current_patient} removed after meeting the doctor")
-elif userinput == "3":
-    print("curret patient queue")
-    for i in patient.queue:
-        print(i)
-elif userinput == "4":
-    print("Exited")
-    
+def main():
+    patient_queue = Queue()
+
+    while True:
+        print("Patient registration and appointment scheduling")
+        print("1. Register patient")
+        print("2. Remove patiet")
+        print("3. Display patiemt queue")
+        print("4. Exit")
+        
+        choice = input("Enter the number you want to select")
+        if choice == "1":
+            register_patient(patient_queue)
+        elif choice == "2":
+            remove_patient(patient_queue)
+        elif choice == "3":
+            display_queue(patient_queue)
+        elif choice == "4":
+            ("logging out")
+            break
+        else:
+            print("Invalid choice")
+def register_patient(queue):
+    patient_name = input("Enter the name of patient:")
+    queue.put(patient_name)
+    print(f"patient'{patient_name}' registered successfully")
+
+def remove_patient(queue):
+    if queue.empty():
+        print("Patient is not in the queue")
+    else:
+        removed_patient = queue.get()
+        print(f"patient'{removed_patient}' removed patient successfully")
+
+def display_queue(queue):
+    if queue.empty():
+        print("No patient in the queue")
+    else:
+        print("Current patient queue")
+        for index, patient in enumerate (list(queue.queue), start = 1):
+            print(f"{index}. {patient}")
+if __name__ == "__main__":
+    main()
